d86bd493740a845f2c9f0ca3c46828b39b48e082
mpasichniuk <marypasichnyuk@gmail.com>
2021-10-14 18:39:34 +0300

    added last task

--- src/main/java/ru/geekbrains/lesson6/Animal.java
+++ src/main/java/ru/geekbrains/lesson6/Animal.java
@@ -4,7 +4,6 @@ package ru.geekbrains.lesson6;
     public final int RUN_LENGTH = 0;
     public final int SWIM_LENGTH = 0;
 
-
     abstract void run(int RUN_LENGTH);
 
     abstract void swim(int SWIM_LENGTH);
--- src/main/java/ru/geekbrains/lesson6/HomeWork.java
+++ src/main/java/ru/geekbrains/lesson6/HomeWork.java
@@ -2,6 +2,10 @@ package ru.geekbrains.lesson6;
 
 public class HomeWork {
     public static void main(String[] args) {
+       int count_cats = 0;
+        int count_dogs = 0;
+        int count_animals = 0;
+
         Cat cat = new Cat();
         cat.run(200);
         cat.swim(0);
@@ -10,5 +14,16 @@ public class HomeWork {
         dog.run(500);
         dog.swim(10);
 
+        count_cats ++;
+             System.out.println("Количество котов: " + count_cats);
+
+        count_dogs ++;
+        System.out.println("Количество собак: " + count_dogs);
+
+        count_animals ++;
+        System.out.println("Количество животных: " +count_animals);
+        }
+
+
     }
-}
+
